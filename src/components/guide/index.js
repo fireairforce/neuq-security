4cc5da5718ca15f104c09297886bfe9e1e07ee51
zoomdong <1344492820@qq.com>
2019-03-10 19:54:53 +0800

    完成引导页面的制作

---
+++ src/components/guide/index.js
@@ -0,0 +1,50 @@
+import React, { Fragment } from 'react'
+import styles from './index.less'
+import { Button,WhiteSpace } from 'antd-mobile'
+
+export default class Guide extends React.Component{
+    constructor(props){
+        super(props)
+        this.state={
+
+        }
+        this.handleClick = this.handleClick.bind(this);
+    }
+    handleClick(item){
+       this.props.history.push(`/${item}`)
+    }
+    render(){
+        const buttonStyle={
+            border:'3px solid #fff',
+            width:"300px",
+            borderRadius:'14px',
+            fontSize:'22px',
+            fontFamily:'Microsoft Yahei',
+            height:'50px',
+            lineHeight:'50px',
+            color:'#fff',
+            margin:'0 auto',
+            pointer:'cursor'
+        }
+        console.log(this.props);
+        return(
+            <Fragment>
+             <div className={styles.wrapper}>
+                <div className={styles.content}>
+                   <p>为了解决校园内车辆混乱的问题,</p>
+                   <p>让师生都能享受良好校园环境，</p>
+                   <p>现收集校内所有车主的信息。</p>
+                   <p>您的信息将会被您所在的学院审核，</p>
+                   <p>之后您会收到车辆出入证。</p>
+                   <p>我们会确保您所提交的信息的安全。</p>
+                   <p>感谢配合。</p>
+                   <WhiteSpace />
+                   <Button type="ghost" style={buttonStyle} onClick={()=>{this.handleClick('collect')}}>电脑填写</Button>
+                   <WhiteSpace /><WhiteSpace /> <WhiteSpace />
+                   <Button type="ghost" style={buttonStyle} onClick={()=>{this.handleClick('mobile')}}>手机填写</Button>
+                </div>
+             </div>
+            </Fragment>
+        )
+    }
+}
--- src/config/route.js
+++ src/config/route.js
@@ -1,28 +1,33 @@
 const routes = [
-    {
-      path:'/',
-      name:'infocollect',
-      models: () => [import('./../models/apply')],
-      component: () => import('./../components/InfoCollect'),
-    },{
-      path:'/checklogin',
-      name:'checklogin',
-      models: () =>[import('./../models/shdl')],
-      component: () => import('./../components/checklogin'),
-    },{
-      path:'/infocheck',
-      name:'infocheck',
-      models: () =>[import('./../models/shyg')],
-      component: () => import('./../components/InfoCheck'),
-    },{
-      path:'/infoall',
-      name:'infoall',
-      models: () =>[import('./../models/xxhz')],
-      component: () => import('./../components/InfoAll'),
-    },{
-      path:'/mobile',
-      name:'mobile',
-      component: () => import('./../components/mobilecollect'),
-    }
-  ]
+  {
+     path:'/',
+     name:'guide',
+     component: ()=> import('./../components/guide') 
+  },
+  {
+    path:'/collect',
+    name:'infocollect',
+    models: () => [import('./../models/apply')],
+    component: () => import('./../components/InfoCollect'),
+  },{
+    path:'/checklogin',
+    name:'checklogin',
+    models: () =>[import('./../models/shdl')],
+    component: () => import('./../components/checklogin'),
+  },{
+    path:'/infocheck',
+    name:'infocheck',
+    models: () =>[import('./../models/shyg')],
+    component: () => import('./../components/InfoCheck'),
+  },{
+    path:'/infoall',
+    name:'infoall',
+    models: () =>[import('./../models/xxhz')],
+    component: () => import('./../components/InfoAll'),
+  },{
+    path:'/mobile',
+    name:'mobile',
+    component: () => import('./../components/mobilecollect'),
+  }
+]
 export default routes
